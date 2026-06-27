f17d8fd03c80f87328c100a26cd8f1061d210bf3
Kian De Guzman <kianchristiand@gmail.com>
2026-06-27 09:09:40 -0500

    blacken UI components that redblack theme missed

--- swe/nvim/lua/config/autocmds.lua
+++ swe/nvim/lua/config/autocmds.lua
@@ -6,3 +6,34 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Make redblack theme more black
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "redblack",
+  callback = function()
+    local pure_black = "#000000"
+
+    local target_groups = {
+      "Normal",
+      "NormalNC",
+      "SignColumn",
+      "NormalFloat",
+      "CursorLineNr",
+      "NeoTreeNormal",
+      "NeoTreeNormalNC",
+      "TelescopeNormal",
+      "LineNr",
+      "FoldColumn",
+      "NonText",
+      "VertSplit",
+    }
+
+    for _, group in ipairs(target_groups) do
+      local current_hl = vim.api.nvim_get_hl(0, { name = group })
+
+      current_hl.bg = pure_black
+
+      vim.api.nvim_set_hl(0, group, current_hl)
+    end
+  end,
+})
